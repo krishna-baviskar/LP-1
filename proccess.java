6250c937b7046c7b71fb025102abf384f00f033a
Krishna Baviskar <krishnabaviskar40@gmail.com>
2025-11-07 03:31:59 +0530

    Update proccess.java

--- proccess.java
+++ proccess.java
@@ -1,61 +1,29 @@
 import java.util.*;
 
-class Process {
-    int pid;
-    int at, bt, ct, tat, wt;
-
-    Process(int pid, int at, int bt) {
-        this.pid = pid;
-        this.at = at;
-        this.bt = bt;
-    }
+class P {
+    int id, at, bt, ct, tat, wt;
+    P(int id,int at,int bt){this.id=id;this.at=at;this.bt=bt;}
 }
 
 public class proccess {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of processes: ");
-        int n = sc.nextInt();
-
-        Process[] p = new Process[n];
-
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter Arrival Time for P" + (i + 1) + ": ");
-            int at = sc.nextInt();
-            System.out.print("Enter Burst Time for P" + (i + 1) + ": ");
-            int bt = sc.nextInt();
-            p[i] = new Process(i + 1, at, bt);
+    public static void main(String[] a) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter no. of processes: ");
+        int n=sc.nextInt(); P[] p=new P[n];
+        for(int i=0;i<n;i++){
+            System.out.print("AT P"+(i+1)+": ");int at=sc.nextInt();
+            System.out.print("BT P"+(i+1)+": ");int bt=sc.nextInt();
+            p[i]=new P(i+1,at,bt);
         }
-
-        // Sort by Arrival Time
-        Arrays.sort(p, Comparator.comparingInt(x -> x.at));
-
-        int currentTime = 0;
-        float totalWT = 0, totalTAT = 0;
-
-        for (int i = 0; i < n; i++) {
-            // If CPU is idle
-            if (currentTime < p[i].at) {
-                currentTime = p[i].at;
-            }
-
-            p[i].ct = currentTime + p[i].bt;              // Completion Time
-            p[i].tat = p[i].ct - p[i].at;                 // Turnaround Time
-            p[i].wt = p[i].tat - p[i].bt;                 // Waiting Time
-
-            currentTime = p[i].ct;
-
-            totalWT += p[i].wt;
-            totalTAT += p[i].tat;
+        Arrays.sort(p,Comparator.comparingInt(x->x.at));
+        int t=0; float twt=0,ttat=0;
+        for(P x:p){
+            if(t<x.at)t=x.at;
+            x.ct=t+x.bt; x.tat=x.ct-x.at; x.wt=x.tat-x.bt; t=x.ct;
+            twt+=x.wt; ttat+=x.tat;
         }
-
         System.out.println("\nPID\tAT\tBT\tCT\tTAT\tWT");
-        for (Process process : p) {
-            System.out.println("P" + process.pid + "\t" + process.at + "\t" + process.bt + "\t" +
-                               process.ct + "\t" + process.tat + "\t" + process.wt);
-        }
-
-        System.out.printf("\nAverage Turnaround Time: %.2f", totalTAT / n);
-        System.out.printf("\nAverage Waiting Time: %.2f\n", totalWT / n);
+        for(P x:p)System.out.println("P"+x.id+"\t"+x.at+"\t"+x.bt+"\t"+x.ct+"\t"+x.tat+"\t"+x.wt);
+        System.out.printf("\nAvg TAT: %.2f\nAvg WT: %.2f\n",ttat/n,twt/n);
     }
 }
