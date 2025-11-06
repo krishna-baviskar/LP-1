3a2d33677f581df45a226df2f8a49cac3c3c0048
Krishna Baviskar <krishnabaviskar40@gmail.com>
2025-11-07 03:28:13 +0530

    Update WorstFit.java

--- WorstFit.java
+++ WorstFit.java
@@ -1,81 +1,37 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class WorstFit {
-
     static class Block {
-        int size;
-        boolean allocated;
-
-        Block(int size) {
-            this.size = size;
-            this.allocated = false;
-        }
-
-        @Override
-        public String toString() {
-            return "[" + size + (allocated ? " Allocated" : " Free") + "]";
-        }
+        int size; boolean alloc=false;
+        Block(int s){size=s;}
+        public String toString(){return"["+size+(alloc?" Allocated":" Free")+"]";}
     }
 
-    static void worstFit(ArrayList<Block> memory, int[] processes) {
-        System.out.println("\nWorst Fit Allocation with Fragmentation:");
-
-        for (int i = 0; i < processes.length; i++) {
-            int worstIdx = -1;
-            for (int j = 0; j < memory.size(); j++) {
-                Block block = memory.get(j);
-                if (!block.allocated && block.size >= processes[i]) {
-                    if (worstIdx == -1 || block.size > memory.get(worstIdx).size) {
-                        worstIdx = j;
-                    }
-                }
+    static void worstFit(List<Block> mem,int[] p){
+        System.out.println("\n--- Worst Fit Allocation ---");
+        for(int i=0;i<p.length;i++){
+            int w=-1;
+            for(int j=0;j<mem.size();j++){
+                Block b=mem.get(j);
+                if(!b.alloc&&b.size>=p[i]&&(w==-1||b.size>mem.get(w).size))w=j;
             }
-
-            if (worstIdx != -1) {
-                Block block = memory.get(worstIdx);
-                if (block.size > processes[i]) {
-                    int leftover = block.size - processes[i];
-                    block.size = processes[i];
-                    block.allocated = true;
-                    memory.add(worstIdx + 1, new Block(leftover));
-                } else {
-                    block.allocated = true;
-                }
-                System.out.println("Process " + (i + 1) + " of size " + processes[i] + " allocated to block " + (worstIdx + 1));
-            } else {
-                System.out.println("Process " + (i + 1) + " of size " + processes[i] + " not allocated");
-            }
+            if(w!=-1){
+                Block b=mem.get(w);
+                if(b.size>p[i]){mem.add(w+1,new Block(b.size-p[i]));b.size=p[i];}
+                b.alloc=true;
+                System.out.println("P"+(i+1)+"("+p[i]+")→B"+(w+1));
+            }else System.out.println("P"+(i+1)+"("+p[i]+") not allocated");
         }
-
-        System.out.println("\nFinal memory blocks state:");
-        for (int i = 0; i < memory.size(); i++) {
-            System.out.println("Block " + (i + 1) + ": " + memory.get(i));
-        }
+        System.out.println("\nFinal Blocks:");
+        for(int i=0;i<mem.size();i++)System.out.println("B"+(i+1)+": "+mem.get(i));
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter number of memory blocks: ");
-        int m = sc.nextInt();
-        ArrayList<Block> memory = new ArrayList<>();
-        System.out.println("Enter sizes of memory blocks:");
-        for (int i = 0; i < m; i++) {
-            System.out.print("Block " + (i + 1) + ": ");
-            memory.add(new Block(sc.nextInt()));
-        }
-
-        System.out.print("\nEnter number of processes: ");
-        int n = sc.nextInt();
-        int[] processes = new int[n];
-        System.out.println("Enter sizes of processes:");
-        for (int i = 0; i < n; i++) {
-            System.out.print("Process " + (i + 1) + ": ");
-            processes[i] = sc.nextInt();
-        }
-
-        worstFit(memory, processes);
+    public static void main(String[]a){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Blocks: ");int m=sc.nextInt();List<Block> mem=new ArrayList<>();
+        for(int i=0;i<m;i++){System.out.print("Size of B"+(i+1)+": ");mem.add(new Block(sc.nextInt()));}
+        System.out.print("Processes: ");int n=sc.nextInt();int[] p=new int[n];
+        for(int i=0;i<n;i++){System.out.print("Size of P"+(i+1)+": ");p[i]=sc.nextInt();}
+        worstFit(mem,p);
     }
 }
-
