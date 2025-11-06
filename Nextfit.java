6a5099c051b21684859bc552201315a4cc8db093
Krishna Baviskar <krishnabaviskar40@gmail.com>
2025-11-07 03:22:45 +0530

    Update Nextfit.java

--- Nextfit.java
+++ Nextfit.java
@@ -1,87 +1,31 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public class Nextfit{
-
-    static class Block {
-        int size;
-        boolean allocated;
-
-        Block(int size) {
-            this.size = size;
-            this.allocated = false;
-        }
-
-        @Override
-        public String toString() {
-            return "[" + size + (allocated ? " Allocated" : " Free") + "]";
-        }
+import java.util.*;
+class NextFit{
+ static class B{int s;boolean a;B(int s){this.s=s;}public String toString(){return"["+s+(a?" Alloc":" Free")+"]";}}
+ static void alloc(List<B>mem,int[]p){
+  int pos=0;
+  for(int i=0;i<p.length;i++){
+   boolean ok=false;int c=0,j=pos;
+   while(c++<mem.size()){
+    B b=mem.get(j);
+    if(!b.a&&b.s>=p[i]){
+     if(b.s>p[i]){int r=b.s-p[i];b.s=p[i];b.a=true;mem.add(j+1,new B(r));}
+     else b.a=true;
+     System.out.println("P"+(i+1)+"("+p[i]+") -> B"+(j+1));
+     pos=(j+1)%mem.size();ok=true;break;
     }
-
-    static void nextFit(ArrayList<Block> memory, int[] processes) {
-        System.out.println("\nNext Fit Allocation with Fragmentation:");
-        int lastPos = 0;
-
-        for (int i = 0; i < processes.length; i++) {
-            boolean allocated = false;
-            int count = 0;
-            int j = lastPos;
-
-            while (count < memory.size()) {
-                Block block = memory.get(j);
-
-                if (!block.allocated && block.size >= processes[i]) {
-                    if (block.size > processes[i]) {
-                        int leftover = block.size - processes[i];
-                        block.size = processes[i];
-                        block.allocated = true;
-                        memory.add(j + 1, new Block(leftover));
-                    } else {
-                        block.allocated = true;
-                    }
-                    System.out.println("Process " + (i + 1) + " of size " + processes[i] + " allocated to block " + (j + 1));
-                    lastPos = (j + 1) % memory.size();
-                    allocated = true;
-                    break;
-                }
-
-                j = (j + 1) % memory.size();
-                count++;
-            }
-
-            if (!allocated) {
-                System.out.println("Process " + (i + 1) + " of size " + processes[i] + " not allocated");
-            }
-        }
-
-        System.out.println("\nFinal memory blocks state:");
-        for (int i = 0; i < memory.size(); i++) {
-            System.out.println("Block " + (i + 1) + ": " + memory.get(i));
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter number of memory blocks: ");
-        int m = sc.nextInt();
-        ArrayList<Block> memory = new ArrayList<>();
-        System.out.println("Enter sizes of memory blocks:");
-        for (int i = 0; i < m; i++) {
-            System.out.print("Block " + (i + 1) + ": ");
-            memory.add(new Block(sc.nextInt()));
-        }
-
-        System.out.print("\nEnter number of processes: ");
-        int n = sc.nextInt();
-        int[] processes = new int[n];
-        System.out.println("Enter sizes of processes:");
-        for (int i = 0; i < n; i++) {
-            System.out.print("Process " + (i + 1) + ": ");
-            processes[i] = sc.nextInt();
-        }
-
-        nextFit(memory, processes);
-    }
+    j=(j+1)%mem.size();
+   }
+   if(!ok)System.out.println("P"+(i+1)+"("+p[i]+") not allocated");
+  }
+  System.out.println("\nFinal Memory:");
+  for(int i=0;i<mem.size();i++)System.out.println("B"+(i+1)+": "+mem.get(i));
+ }
+ public static void main(String[]a){
+  Scanner s=new Scanner(System.in);
+  System.out.print("Blocks: ");int m=s.nextInt();List<B>mem=new ArrayList<>();
+  for(int i=0;i<m;i++)mem.add(new B(s.nextInt()));
+  System.out.print("Processes: ");int n=s.nextInt(),p[]=new int[n];
+  for(int i=0;i<n;i++)p[i]=s.nextInt();
+  alloc(mem,p);
+ }
 }
-
